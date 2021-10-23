cf5a19568ecbeed5446d1c65e3208c8c82367534
ekibragimova <91051569+ekibragimova@users.noreply.github.com>
2021-10-23 17:31:28 +0500

    задание

---
+++ задание3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            Console.WriteLine("enterx =");
+            Console.WriteLine(value: y = conclusion) ;
+            Console.ReadKey();
+        }
+        static double conclusion
+        {
+            get
+            {
+                double y;
+                var x = double.Parse(Console.ReadLine());
+                return y = 1 + Math.Sqrt((Math.Pow(x, 2) - 1) / (Math.Pow(x, 2) + 1));
+            }
+        }
+            
+        }
+}
+
