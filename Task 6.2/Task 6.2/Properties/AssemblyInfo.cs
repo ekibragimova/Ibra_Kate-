3c7e97597155c221c8c7fdcc61a355afb46c3b66
ekibragimova <91051569+ekibragimova@users.noreply.github.com>
2021-11-28 21:02:48 +0500

    task 6.2

--- Task 6.2/Task 6.2/Properties/AssemblyInfo.cs
+++ Task 6.2/Task 6.2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Task 7")]
+[assembly: AssemblyTitle("Task 6.2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task 7")]
+[assembly: AssemblyProduct("Task 6.2")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("91f7139b-8712-469c-b910-45171ebdfdb4")]
+[assembly: Guid("b682b80f-13ee-4f19-a6fd-1cf1d27cc3f7")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
