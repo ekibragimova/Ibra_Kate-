9dc9167251a5968c0a8218fd6cd947951bf303fe
ekibragimova <91051569+ekibragimova@users.noreply.github.com>
2021-12-30 15:06:21 +0500

    tdd

--- task4fortest/task4fortest/Program.cs
+++ task4fortest/task4fortest/Program.cs
@@ -11,29 +11,32 @@ namespace task4fortest
         static void Main(string[] args)
         {
             var number = 1;
-            var p = 0;
-            int mistake=0;
+            int mistake = 0;
             while (mistake != 1)
             {
-                number +=2;
-                    for (var m =1; m < number; m++)
+                for (int p = 1; p <= number; p++)
+                {
+                    if (TheSpherOfEratosphen(p) == true)
+                        number += 2;
+                    for (var m = 1; m < number; m++)
                     {
-                        if ( number == TheSpherOfEratosphen(p) + 2 * m * m)
+                        if (number == p + 2 * m * m)
                             break;
                         else;
                         Console.WriteLine(number);
-                        mistake=1;
+                        mistake = 1;
                     }
+                }
             }
-            static bool TheSpherOfEratosphen (int a)
+            static bool TheSpherOfEratosphen(int a)
             {
-                int c=1;
-                for (int i=2; i <= Math.Sqrt(a); i++)
+                int c = 1;
+                for (int i = 2; i <= Math.Sqrt(a); i++)
                 {
                     if (a % i == 0)
                     {
                         c = 0;
-                        break; 
+                        break;
                     }
                 }
                 return (c == 1);
