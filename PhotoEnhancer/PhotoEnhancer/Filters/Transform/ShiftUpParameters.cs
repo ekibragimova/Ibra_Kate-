d543ac0c2c7c4e9fc3ead91262d5a959d6069c17
ekibragimova <91051569+ekibragimova@users.noreply.github.com>
2023-06-11 22:02:58 +0500

    ,mnmkj

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Transform/ShiftUpParameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class ShiftUpParameters : IParameters
+    {
+        public double ShiftPercent { get; set; }
+        public ParameterInfo[] GetDecription()
+        {
+            return new[]
+            {
+                new ParameterInfo()
+                {
+                    Name = "Процент сдвига",
+                    MinValue = 0,
+                    MaxValue = 100,
+                    DefaultValue = 0,
+                    Increment = 5
+                }
+            };
+        }
+
+        public void SetValues(double[] values)
+        {
+            ShiftPercent = values[0];
+        }
+    }
+}
