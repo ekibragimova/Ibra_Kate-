3c84ea87decf25174718625952857578fdc1db01
ekibragimova <91051569+ekibragimova@users.noreply.github.com>
2023-06-12 22:51:38 +0500

    лоил

---
+++ UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/LighteningFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class LighteningFilter : PixelFilter
+    {
+        public LighteningFilter() : base(new LighteningParameters()) { }
+
+        public override Pixel ProcessPixel(Pixel pixel, IParameters parameters)
+        {
+            return pixel * (parameters as LighteningParameters).Coefficient;
+        }
+
+        public override string ToString()
+        {
+            return "Осветление/затемнение";
+        }
+    }
+}
---
+++ UGI-216004-main/PhotoEnhancer/PhotoEnhancer/Filters/Transform/ShiftUpTransformer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class ShiftUpTransformer : ITransformer<ShiftUpParameters>
+    {
+        double border { get; set; }
+
+        public Size ResultSize { get; private set; }
+
+        public void Initialize(Size size, ShiftUpParameters parameters)
+        {
+            ResultSize = size;
+            border = size.Height - Math.Round(parameters.ShiftPercent / 100 * size.Height);
+        }
+
+        public Point? MapPoint(Point point)
+        {
+            int x, y;
+            x = point.X;
+            if (point.Y < border)
+                y = point.Y + (int)(ResultSize.Height - border);
+            else
+                y = point.Y - (int)border;
+
+            if (y < 0 || y >= ResultSize.Height)
+                return null;
+
+            return new Point(x, y);
+        }
+    }
+}
